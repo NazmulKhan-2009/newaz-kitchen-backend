b5e69a553d65d31a850ce500d11c4e943944053f
NazmulKhan-2009 <nazmulustc09@gmail.com>
2021-07-04 17:36:54 +0600

    email verification coded

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,78 +2,194 @@ const UserRegister=require('../models/user')
 const admin=require('../models/admin')
 const bcrypt=require('bcrypt')
 const jwt = require('jsonwebtoken');
+const mailgun = require("mailgun-js");
+const DOMAIN = 'sandbox29e352940d3f4436ab3570871da6bd7c.mailgun.org';
+// const DOMAIN = 'mail.newazkitchenbdapi.com'; //!not working
 
+
+// const createUser = async(req,res)=>{
+  
+//  try{
+//   //  const hash_pass=req.body.user_password===req.body.confirm_password?
+//     // await bcrypt.hash(req.body.user_password, 10):false
+
+//     const hash_pass=await bcrypt.hash(req.body.user_password, 10)
+
+//   const userData={
+//     user_name:req.body.user_name,
+//     user_email:req.body.user_email,
+//     user_phone:req.body.user_phone,
+//     user_password:hash_pass,
+//     local_date:new Date().toDateString(),
+//     local_time:new Date().toLocaleTimeString(),
+//   }
+
+//   // const userInfo=new UserRegister(userData)
+//   // await userInfo.save()
+  
+//   // confirm password validation from db
+//   // const responsedData=hash_pass?await userInfo.save():{error:"password combination mismatch"}
+// const existUser=await UserRegister.findOne({user_name:userData.user_name})
+// const existEmail=await UserRegister.findOne({user_email:userData.user_email})
+// const existPhone=await UserRegister.findOne({user_phone:userData.user_phone})
+// const adminEmail=await admin.findOne({admin_email:userData.user_email})
+// const adminName=await admin.findOne({admin_name:userData.user_name})
+
+// // console.log(existUser)
+// // console.log(adminInfo)
+
+// // if(existUser){
+// //   res.status(400).json({
+// //     res:"user name exist"
+// //   })
+// // }else{
+// //   res.status(200).json("okay")
+// // }
+// if(existUser){
+//   res.status(200).json({
+    
+//     response:`${existUser.user_name} already Exist`
+//   })
+// }else if(existEmail){
+//   res.status(200).json({
+//     response:`Already registered with ${existEmail.user_email} try another one`
+//   })
+// }else if(existPhone){
+//   res.status(200).json({
+//     response:`Already registered with ${existPhone.user_phone} try another one`
+//   })
+// }else if(adminEmail){
+//   res.status(200).json({
+//     response:`Already registered as an admin`
+//   })
+// }else if(adminName){
+//   res.status(200).json({
+//     response:`title Already registered as an admin`
+//   })
+// }else{
+//   const data=await UserRegister.create(userData)
+//   res.status(200).json({
+//     response: `Welcome ${data.user_name} , you Signed Up Succesfully`,
+//   })
+// }
+ 
+//   }catch(e){
+//     res.status(400).send(e)
+//   } ;
+// }
+
+//! SIGN UP WITH EMAIL VERIFICATION CODE STARTED
 
 const createUser = async(req,res)=>{
   
- try{
-  //  const hash_pass=req.body.user_password===req.body.confirm_password?
-    // await bcrypt.hash(req.body.user_password, 10):false
-
-    const hash_pass=await bcrypt.hash(req.body.user_password, 10)
-
-  const userData={
-    user_name:req.body.user_name,
-    user_email:req.body.user_email,
-    user_phone:req.body.user_phone,
-    user_password:hash_pass,
-    local_date:new Date().toDateString(),
-    local_time:new Date().toLocaleTimeString(),
-  }
-
-  // const userInfo=new UserRegister(userData)
-  // await userInfo.save()
-  
-  // confirm password validation from db
-  // const responsedData=hash_pass?await userInfo.save():{error:"password combination mismatch"}
-const existUser=await UserRegister.findOne({user_name:userData.user_name})
-const existEmail=await UserRegister.findOne({user_email:userData.user_email})
-const existPhone=await UserRegister.findOne({user_phone:userData.user_phone})
-const adminEmail=await admin.findOne({admin_email:userData.user_email})
-const adminName=await admin.findOne({admin_name:userData.user_name})
-
-// console.log(existUser)
-// console.log(adminInfo)
-
-// if(existUser){
-//   res.status(400).json({
-//     res:"user name exist"
-//   })
-// }else{
-//   res.status(200).json("okay")
-// }
-if(existUser){
-  res.status(200).json({
-    
-    response:`${existUser.user_name} already Exist`
-  })
-}else if(existEmail){
-  res.status(200).json({
-    response:`Already registered with ${existEmail.user_email} try another one`
-  })
-}else if(existPhone){
-  res.status(200).json({
-    response:`Already registered with ${existPhone.user_phone} try another one`
-  })
-}else if(adminEmail){
-  res.status(200).json({
-    response:`Already registered as an admin`
-  })
-}else if(adminName){
-  res.status(200).json({
-    response:`title Already registered as an admin`
-  })
-}else{
-  const data=await UserRegister.create(userData)
-  res.status(200).json({
-    response: `Welcome ${data.user_name} , you Signed Up Succesfully`,
-  })
-}
+  try{
+   //  const hash_pass=req.body.user_password===req.body.confirm_password?
+     // await bcrypt.hash(req.body.user_password, 10):false
  
-  }catch(e){
-    res.status(400).send(e)
-  } ;
+     const hash_pass=await bcrypt.hash(req.body.user_password, 10)
+ 
+   const userData={
+     user_name:req.body.user_name,
+     user_email:req.body.user_email,
+     user_phone:req.body.user_phone,
+     user_password:hash_pass,
+     local_date:new Date().toDateString(),
+     local_time:new Date().toLocaleTimeString(),
+   }
+ 
+   // const userInfo=new UserRegister(userData)
+   // await userInfo.save()
+   
+   // confirm password validation from db
+   // const responsedData=hash_pass?await userInfo.save():{error:"password combination mismatch"}
+ const existUser=await UserRegister.findOne({user_name:userData.user_name})
+//  const existEmail=await UserRegister.findOne({user_email:userData.user_email})
+ const existPhone=await UserRegister.findOne({user_phone:userData.user_phone})
+ const adminEmail=await admin.findOne({admin_email:userData.user_email})
+ const adminName=await admin.findOne({admin_name:userData.user_name})
+ 
+ // console.log(existUser)
+ // console.log(adminInfo)
+ 
+ // if(existUser){
+ //   res.status(400).json({
+ //     res:"user name exist"
+ //   })
+ // }else{
+ //   res.status(200).json("okay")
+ // }
+ if(existUser){
+   res.status(200).json({
+     
+     response:`${existUser.user_name} already Exist`
+   })
+ }
+//  else if(existEmail){
+//    res.status(200).json({
+//      response:`Already registered with ${existEmail.user_email} try another one`
+//    })
+//  }
+ else if(existPhone){
+   res.status(200).json({
+     response:`Already registered with ${existPhone.user_phone} try another one`
+   })
+ }else if(adminEmail){
+   res.status(200).json({
+     response:`Already registered as an admin`
+   })
+ }else if(adminName){
+   res.status(200).json({
+     response:`title Already registered as an admin`
+   })
+ }else{
+  //  const data=await UserRegister.create(userData)
+  //  res.status(200).json({
+  //    response: `Welcome ${data.user_name} , you Signed Up Succesfully`,
+  //  })
+ 
+  //!------------>
+const mg = mailgun({apiKey: '946ff4b1dc204b52b7e7e5b7f664d8ac-1f1bd6a9-df73d674', domain: DOMAIN});
+// const token=jwt.sign({userName:userData.user_name,userEmail:userData.user_email},'mynameiskhan',{expiresIn:'60m'})
+const token=jwt.sign(userData,'mynameiskhan',{expiresIn:'60m'})
+
+const data = {
+	from: 'noreply@hello.com',
+	to: userData.user_email,
+	subject: 'Ac Activation',
+  html:`
+  <h2>Activate Your Account</h2>
+  <a style="cursor:pointer; color:'tomato" href='http://localhost:5000/api/user/activeacc/${token}'>Activate Your Account</a>
+  `
+};
+mg.messages().send(data, function (error, body) {
+	error ? res.status(200).json({response:'Something wrong'}) 
+  : res.status(200).json({
+       response: `Welcome ${userData.user_email} , you Signed Up Succesfully`,
+     })
+	console.log({body});
+});
+ }
+  
+   }catch(e){
+     res.status(400).send(e)
+   } ;
+ }
+ 
+
+const verifiedUser=async(req,res)=>{
+    // console.log(req.params.signupinfo)
+    // res.status(200).json({user_info:req.params.signupinfo})
+    const verifiedData=jwt.verify(req.params.signupinfo, 'mynameiskhan')
+    console.log(verifiedData)
+    // res.status(200).json(verifiedData)
+   await UserRegister.create(verifiedData)
+
+    res.redirect(`http://localhost:3000/verified/${req.params.signupinfo}`)
+    
 }
+
+
+//! SIGN UP WITH EMAIL VERIFICATION CODE FINISHED
 
 //SIGN IN
 const signIn=async(req,res)=>{
@@ -180,6 +296,7 @@ const userList=async(req,res)=>{
 module.exports={
  createUser,
  signIn,
- userList
+ userList,
+ verifiedUser
 
 }
--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -14,4 +14,8 @@ router
 .route('/alluser')
 .get(userController.userList)
 
+router
+.route('/activeacc/:signupinfo')
+.get(userController.verifiedUser)
+
 module.exports=router
